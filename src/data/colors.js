cfbb07fd956146c31d20b0e192da0ff919e84893
Chewam <gary@chewam.com>
2020-10-25 05:48:47 +0100

    feat: Dynamic chart color scheme.

---
+++ src/data/colors.js
@@ -0,0 +1,11 @@
+import deaths from "@data/deaths"
+import palette from "google-palette"
+
+const keys = Object.keys(deaths[0])
+
+const colors = palette("rainbow", keys.length, 0, 0.5)
+
+export default keys.reduce(
+  (res, year, i) => (year !== "month" && (res[year] = `#${colors[i]}`), res),
+  {}
+)
