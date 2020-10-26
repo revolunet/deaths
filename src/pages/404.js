021a7ad3cb89f5d012963800e34666bce534cbf2
Chewam <gary@chewam.com>
2020-10-26 23:50:52 +0100

    chore: Add Lighthouse CI workflow. (#44)
    * Add Lighthouse workflow.
    
    * Use Node 10 for Lighthouse job.
    
    * Add Lighthouse project config.
    
    * Add Lighthouse staticDistDir config.
    
    * Fix Lighthouse staticDistDir config.
    
    * Fix Lighthouse staticDistDir path.
    
    * Reference LHCI_GITHUB_APP_TOKEN in lighthouse job.
    
    * Add Lighthouse assertions preset.
    
    * Add Lighthouse assertions excluding PWA.
    
    * Add missing HEAD to 404 page.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,3 +1,17 @@
-export default function Custom404() {
-  return <h1>404 - Page Not Found</h1>
-}
+import Head from "next/head"
+
+const Custom404 = () => (
+  <>
+    <Head>
+      <title>Décès annuels en France</title>
+      <meta
+        name="description"
+        property="og:description"
+        content="Statistiques annuelles des décès en France de 2010 à nos jours, basées sur les données de l'INSEE."
+      />
+    </Head>
+    <h1>404 - Page Not Found</h1>
+  </>
+)
+
+export default Custom404
