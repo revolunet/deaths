921d0a92492d63056d2c910c09f8aa09ed92456e
Chewam <gary@chewam.com>
2021-02-15 23:09:35 +0100

    fix: Add @semantic-release/git.

--- scripts/build.js
+++ scripts/build.js
@@ -5,25 +5,10 @@ var es = require("event-stream")
 var process = require("process")
 const crypto = require("crypto")
 const isValid = require("date-fns/isValid")
+const Months = require("../src/data/months.json")
 const intervalToDuration = require("date-fns/intervalToDuration")
 
 const baseDir = `${__dirname}/../data`
-
-const Months = require("../src/data/months.json")
-
-// const ageGroups = [
-//   [0, 9],
-//   [10, 19],
-//   [20, 29],
-//   [30, 39],
-//   [40, 49],
-//   [50, 59],
-//   [60, 69],
-//   [70, 79],
-//   [80, 89],
-//   [90, 99],
-//   [100, 1000],
-// ]
 
 const data = new Map()
 
